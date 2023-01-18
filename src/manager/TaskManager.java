92bd874a0c8a72e4545d4c56eacbd17c4d629efe
PolinKartav <polyakarpushina@yandex.ru>
2023-01-18 16:38:39 +0300

    second

---
+++ src/manager/TaskManager.java
@@ -0,0 +1,41 @@
+package manager;
+
+import tasks.Subtask;
+
+import java.util.ArrayList;
+
+public interface TaskManager {
+    //1. Метод для хранения всех задач
+    void saveAllTasks(Object o);
+
+    //2.1 Получение списка всех задач.
+    ArrayList<Object> getListOfAllTasks();
+
+    //2.2 Удаление всех задач;
+    void deleteTasks(ArrayList<Object> list);
+
+    //2.3 Получение по идентификатору;
+    Object getTaskById(int id);
+
+    //2.4 Создание. Сам объект должен передаваться в качестве параметра;
+    Object createTask(Object o);
+
+    //2.5 Обновление. Новая версия объекта с верным идентификатором передаются в виде параметра;
+    void updateTasks(Object o, int id);
+
+    //обновление Subtasks в Epic
+     void updateSubtaskInEpic(Subtask subtask, int id);
+
+    //2.6 Удаление по идентификатору.
+    void removeTaskById(Integer id);
+
+    //удаление Subtasks в Epic
+    void removeSubtasksInEpic(int id);
+
+    //3. Дополнительные методы:
+    //3.1 Получение списка всех подзадач определённого эпика.
+    ArrayList<Subtask> getAllSubtsksOfEpic(int id);
+
+    //4. Метод для управления статусом для эпик задач.
+    void getStatusEpic();
+}
